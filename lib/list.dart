4578bf639826bf77d6485efe5693b694a8389b2a
makiyoi <kazahana05acidmoon.5@gmail.com>
2024-02-26 17:23:09 +0900

    edit

--- lib/list.dart
+++ lib/list.dart
@@ -3,7 +3,6 @@ import 'package:flutter_advanced_2/provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_advanced_2/search.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_advanced_2/models.dart';
 
 class List extends ConsumerWidget {
     const List({super.key});
@@ -26,40 +25,44 @@ class List extends ConsumerWidget {
         ],
         backgroundColor: Colors.brown,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            booksStream.when(
-              error: (err, _) => Text(err.toString()),
-              loading: () => const CircularProgressIndicator(),
-              data:(selectBooks) { //QuerySnapshotはdocumentSnapshotの集まり
-                 final data = selectBooks.docs; //documentSnapshotのリストを取得する
-                final filter = ref.watch(listFilterProvider);//フィルターを監視する
-                 final keywordBook = data.where((book) =>  book['explanation'].contains(filter)).toList();
-                 //リストの中の本の内容(explanation)にfilterが含まれるものを取得する。変数に代入
-                 //keywordBook.where(() => ).toList();
-                 return Expanded(
-                     child: ListView.builder(
-                         itemCount: keywordBook.length,
-                         itemBuilder: (context, index) {
-                           return Card(
-                             child: ListTile(
-                               leading: const Icon(Icons.book),
-                               title: Text(keywordBook[index]['title'] + '-' + keywordBook[index]['author'],
-                                   style: const TextStyle(fontWeight: FontWeight.bold)),
-                               subtitle: Text(keywordBook[index]['explanation']),
-                             ),
-                           );
-                         }
-                         )
-                 );
-              },
-            )
-          ],
+      body: Consumer(
+        builder: (context, ref, _) {
+          return
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  booksStream.when(
+                    error: (err, _) => Text(err.toString()),
+                    loading: () => const CircularProgressIndicator(),
+                    data:(selectBooks) { //QuerySnapshotはdocumentSnapshotの集まり
+                        final data = selectBooks.docs; //documentSnapshotのリストを取得する
+                        final filter = ref.watch(listFilterProvider);//フィルターを監視する
+                        final keywordBook = data.where((book) =>  book['explanation'].contains(filter)).toList();
+                       //リストの中の本の内容(explanation)にfilterが含まれるものを取得する。変数に代入
+                        return Expanded(
+                            child: ListView.builder(
+                                itemCount: keywordBook.length,
+                                itemBuilder: (context, index) {
+                                  return Card(
+                                    child: ListTile(
+                                      leading: const Icon(Icons.book),
+                                      title: Text(keywordBook[index]['title'] + '-' + keywordBook[index]['author'],
+                                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                                      subtitle: Text(keywordBook[index]['explanation']),
+                                    ),
+                                  );
+                                },
+                                ),
+                        );
+                        },
+                  ),
+                ],
+              ),
+            );
+        },
         ),
-      ),
-     );
+    );
   }
 }
 
